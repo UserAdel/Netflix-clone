4cf7e8a76a2a73dcf1ede0560cb8c59c17c2deee
adel hany <adel58420@gmail.com>
2025-03-14 19:57:21 +0200

    Deploy

--- frontend/src/Pages/Home/HomeScreen.jsx
+++ frontend/src/Pages/Home/HomeScreen.jsx
@@ -63,14 +63,14 @@ const HomeScreen = () => {
           </div>
           <div className='flex mt-8'>
             <Link
-              to={`/watch/123`}
+              to={`/watch/${trendingContent.id}`}
               className='bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex items-center'
             >
               <Play className='size-6 mr-2 fill-black' />
               Play
             </Link>
             <Link
-              to={`/watch/123`}
+             to={`/watch/${trendingContent.id}`}
               className='bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center'
             >
               <Info className='size-6 mr-2' />
